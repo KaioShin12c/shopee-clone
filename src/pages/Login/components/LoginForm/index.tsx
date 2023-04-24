ab16367cc00ac44dbbd810350da408da32e4e325
KaioShin12c <nguyentiendat12c@gmail.com>
2023-04-24 22:06:05 +0700

    validate register and login form, setup axios and react-query

--- src/pages/Login/components/LoginForm/index.tsx
+++ src/pages/Login/components/LoginForm/index.tsx
@@ -1,25 +1,58 @@
 import Button from "@/components/Button";
 import Input from "@/components/Input";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+interface Login {
+  email: string;
+  password: string;
+}
+
+const schema = yup.object({
+  email: yup.string().required("Trường này là bắt buộc").email("Định dạng phải là email"),
+  password: yup.string().required("Trường này là bắt buộc"),
+});
 
 const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Login>({ resolver: yupResolver(schema) });
+
+  function onSubmit(data: Login) {
+    console.log(data);
+  }
+
   return (
-    <div className=" bg-white p-8 w-[400px]">
-      <h1 className="text-lg mb-8 text-primary-black">Đăng nhập</h1>
-      <Input placeholder="Enter your email" />
-      <Input placeholder="Enter your password" />
-      <Button type="submit" className="w-full bg-primary">
-        Tiếp theo
-      </Button>
-      <div className="text-center">
-        <span className="text-sm text-[rgba(0,0,0,.26)]">
-          Bạn mới biết đến Shopee?
-          <Link className="text-primary" to="/register">
-            Đăng ký
-          </Link>
-        </span>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className=" bg-white p-8 w-[400px]">
+        <h1 className="text-lg mb-8 text-primary-black">Đăng nhập</h1>
+        <Input
+          placeholder="Enter your email"
+          errorMessage={errors.email?.message}
+          {...register("email")}
+        />
+        <Input
+          placeholder="Enter your password"
+          errorMessage={errors.password?.message}
+          {...register("password")}
+        />
+        <Button type="submit" className="w-full bg-primary">
+          Tiếp theo
+        </Button>
+        <div className="text-center">
+          <span className="text-sm text-[rgba(0,0,0,.26)]">
+            Bạn mới biết đến Shopee?
+            <Link className="text-primary" to="/register">
+              Đăng ký
+            </Link>
+          </span>
+        </div>
       </div>
-    </div>
+    </form>
   );
 };
 
--- src/pages/Register/components/RegisterForm/index.tsx
+++ src/pages/Register/components/RegisterForm/index.tsx
@@ -1,27 +1,45 @@
 import Button from "@/components/Button";
 import Input from "@/components/Input";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+interface Register {
+  email: string;
+  password: string;
+}
 
 const RegisterForm = () => {
-  return (
-    <div className=" bg-white p-8 w-[400px]">
-      <h1 className="text-lg mb-8 text-primary-black">Đăng ký</h1>
-      <Input placeholder="Enter your email" />
-      <Input placeholder="Enter your password" />
-      <Input placeholder="Enter your confirm-password" />
-      <Button type="submit" className="w-full bg-primary">
-        Tiếp theo
-      </Button>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Register>();
 
-      <div className="text-center">
-        <span className="text-sm text-[rgba(0,0,0,.26)]">
-          Bạn đã có tài khoản?
-          <Link className="text-primary" to="/login">
-            Đăng nhập
-          </Link>
-        </span>
+  function onSubmit(data: Register) {
+    console.log("data", data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className=" bg-white p-8 w-[400px]">
+        <h1 className="text-lg mb-8 text-primary-black">Đăng ký</h1>
+        <Input placeholder="Enter your email" {...register("email")} />
+        <Input placeholder="Enter your password" {...register("password")} />
+        <Input placeholder="Enter your confirm-password" />
+        <Button type="submit" className="w-full bg-primary">
+          Tiếp theo
+        </Button>
+
+        <div className="text-center">
+          <span className="text-sm text-[rgba(0,0,0,.26)]">
+            Bạn đã có tài khoản?
+            <Link className="text-primary" to="/login">
+              Đăng nhập
+            </Link>
+          </span>
+        </div>
       </div>
-    </div>
+    </form>
   );
 };
 
